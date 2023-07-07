8769a916062066ff92d661ab99cd4f6a679f519e
Nicholas Denaro <ndenarodev@gmail.com>
2023-07-07 13:03:00 -0400

    base project

---
+++ src/game.ts
@@ -0,0 +1,103 @@
+import { Canvas2DView, ControllerBinding, Engine, FixedTickEngine, KeyboardController, Scene, MouseController, Sprite, Sound, GamepadController } from 'game-engine';
+import { Player } from './player';
+
+const screenWidth = 240;
+const screenHeight = 160;
+const scale = 3;
+
+const engine: Engine = new FixedTickEngine(60);
+
+const spriteAssets = require.context('../assets/', false, /\.png$/);
+const spriteAssetsPremade = require.context('../assets/premade', false, /\.png$/);
+new Sprite('buddy', spriteAssets('./buddy.png'), { spriteWidth: 64, spriteHeight: 96 });
+const wavAssets = require.context('../assets/', false, /\.wav$/);
+const wavAssetsPremade = require.context('../assets/premade', false, /\.wav$/);
+new Sound('start', wavAssetsPremade('./GAME_MENU_SCORE_SFX001416.wav'));
+
+async function init() {
+
+  await Sprite.waitForLoad();
+
+  const view = new Canvas2DView(screenWidth, screenHeight, { scale: scale, bgColor: '#BBBBBB' });
+  const scene = new Scene(view);
+  scene.addController(new KeyboardController(keyMap));
+  scene.addController(new MouseController(mouseMap));
+  scene.addController(new GamepadController(gamepadMap));
+  const scenePause = new Scene(view);
+
+  engine.addScene('main', scene);
+  engine.addScene('pause', scenePause);
+
+  scene.addEntity(new Player());
+
+  engine.switchToScene('main');
+
+  Sound.setVolume(0.1);
+
+  Sound.Sounds['start'].play();
+
+  await engine.start();
+}
+
+const keyMap = [
+  {
+    binding: new ControllerBinding<undefined>('left'),
+    keys: ['ArrowLeft', 'a', 'A'],
+  },
+  {
+    binding: new ControllerBinding<undefined>('right'),
+    keys: ['ArrowRight', 'd', 'D'],
+  },
+  {
+    binding: new ControllerBinding<undefined>('up'),
+    keys: ['ArrowUp', 'w', 'W'],
+  },
+  {
+    binding: new ControllerBinding<undefined>('down'),
+    keys: ['ArrowDown', 's', 'S'],
+  },
+  {
+    binding: new ControllerBinding<undefined>('shift'),
+    keys: ['Shift'],
+  },
+  {
+    binding: new ControllerBinding<undefined>('action'),
+    keys: [' '],
+  },
+];
+
+const mouseMap = [
+  {
+    binding: new ControllerBinding<{ x: number, y: number, dx: number, dy: number }>('interact'),
+    buttons: [0],
+  }
+];
+
+const gamepadMap = [
+  {
+    binding: new ControllerBinding<{ value: number }>('button1'),
+    buttons: [
+      { type: 'buttons', index: 0 },
+    ],
+  },
+  {
+    binding: new ControllerBinding<{ value: number }>('button2'),
+    buttons: [
+      { type: 'buttons', index: 1 },
+    ],
+  },
+  {
+    binding: new ControllerBinding<{ value: number }>('axis1'),
+    buttons: [
+      { type: 'axes', index: 0 },
+    ],
+  },
+  {
+    binding: new ControllerBinding<{ value: number }>('axis2'),
+    buttons: [
+      { type: 'axes', index: 1 },
+    ],
+  }
+];
+
+init();
---
+++ src/player.ts
@@ -0,0 +1,10 @@
+import { ControllerState, Scene, Sound, Sprite, SpriteEntity, SpritePainter } from "game-engine";
+
+export class Player extends SpriteEntity {
+  constructor() {
+    super(new SpritePainter(Sprite.Sprites['buddy']), 0, 0);
+  }
+
+  tick(scene: Scene): Promise<void> | void {
+  }
+}
